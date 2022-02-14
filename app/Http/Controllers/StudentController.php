d120c85034d1a7b35202326c1b108049b820c83d
ShihabUddinAhmed <shihabahmed8957@gmail.com>
2022-02-15 05:00:09 +0600

    Function to give 20 records each time

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
     public function studentList(Request $req)
     {
         $pageNumber = $req->pageNumber;
-        return $pageNumber;
+        $users = Student::all()->skip(($pageNumber-1)*20)->take(20);
+        return $users;
     }
 }
